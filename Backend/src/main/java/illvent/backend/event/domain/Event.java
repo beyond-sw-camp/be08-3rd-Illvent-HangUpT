4105c109c66afc4b41bb26db3df789d0416e6bbb
박성준 <macleod.park@gmail.com>
2024-08-16 21:08:51 +0900

    feat. event entity

--- Backend/src/main/java/illvent/backend/event/domain/Event.java
+++ Backend/src/main/java/illvent/backend/event/domain/Event.java
@@ -1,4 +1,39 @@
 package illvent.backend.event.domain;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Event")
 public class Event {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no")
+    private Long no;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String price;
+
+
 }
