be1c8471d32ae6509a88ad4b299d2e5950a69a4f
박성준 <macleod.park@gmail.com>
2024-08-16 18:12:16 +0900

    feat. 행사 상세정보 1차 구현

---
+++ Frontend/HangUpT/src/components/EventDetails.vue
@@ -0,0 +1,181 @@
+<template>
+    <div class="event-details-container">
+        <header class="event-header">
+            
+        </header>
+        <section class="sidebar">
+            <p>{{event.title }}</p>
+            <img src="../assets/list/1.jpg"><br>
+            <a :href="event.registrationLink" class="register-button">신청하기</a>
+            <a :href="event.inquiryLink" class="inquiry-button">관심 행사</a>
+        </section>
+        <section class="event-content">
+            <!-- <img :src="getImageUrl(event.bannerImage)" class="event-banner" alt="Event Banner"/> -->
+            <img src="https://eventusstorage.blob.core.windows.net/evs/Image/sparklabs-demoday12/88048/ProjectInfo/Cover/29a292e4e86e44cebe25ad72929b975e.jpg" 
+            clss="event-banner" alt="Event Banner">
+            <div class="event-info">
+                <div class="event-details">
+                    <h2>행사 정보</h2>
+                    <ul>
+                        <li><strong>일시</strong> {{ event.date }} </li>
+                        <li><strong>비용</strong> {{ event.price }}</li>
+                        <li><strong>장소</strong> {{ event.location }}</li>
+                        <li><strong>주소</strong> {{ event.address }} </li>
+                        <!-- <li><strong>Organizer</strong> {{ event.organizer }}</li>
+                            <li><strong>Speakers</strong> {{ event.speakers }}</li> -->
+                    </ul>
+                </div>
+                <div v-for="(des, i) in event.description" :key="i">
+                    <p class="event-description" >{{ des }}</p>
+                </div>
+            </div>
+        </section>
+        
+        <footer class="event-footer">
+        </footer>
+    </div>
+</template>
+    
+<script>
+    import { defineComponent } from 'vue';
+    import EventData from '../assets/EventData.js'; // 데이터 경로에 맞게 수정하세요
+
+export default defineComponent({
+    name: 'EventDetails',
+    data() {
+        return {
+            event: EventData
+        };
+    },
+methods: {
+    getImageUrl(url) {
+        return new URL(`${url}`, import.meta.url).href;
+        }
+    }
+});
+</script>
+
+<style>
+.event-details-container {
+    float: left;
+    width: 1400px;
+    margin-top: 100px;
+    padding: 20px;
+    align-items: left;
+    /* padding-right: 400px; */
+    padding-left: 100px;
+    height: 2000px;
+}
+
+.event-header {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.event-title {
+    font-size: 2rem;
+    margin: 0;
+}
+
+.event-meta {
+    color: grey;
+}
+
+.event-date,
+.event-location {
+    margin: 5px 0;
+}
+
+.event-content {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    padding-right: 30px;
+}
+
+.event-banner {
+    width: 100%;
+    height: auto;
+    max-height: 400px;
+    object-fit: cover;
+    margin-bottom: 20px;
+}
+
+.event-info {
+    /* max-width: 800px; */
+    text-align: center;
+    padding-top: 200px;
+}
+
+.event-description {
+    font-size: 1.2rem;
+    margin-bottom: 20px;
+    padding-top: 30px;
+    text-align: left;
+}
+
+.event-details {
+    text-align: left;
+}
+
+.event-details h2 {
+    font-size: 1.5rem;
+}
+
+.event-details ul {
+    list-style: none;
+    padding: 0;
+}
+
+.event-details li {
+    margin-bottom: 10px;
+    padding-top: 10px;
+}
+
+.event-details li strong {
+    padding-right: 30px;
+}
+
+.event-footer {
+    text-align: center;
+    margin-top: 20px;
+}
+
+.register-button,
+.inquiry-button {
+    display: inline-block;
+    padding: 10px 20px;
+    margin: 10px;
+    color: white;
+    text-decoration: none;
+    border-radius: 5px;
+    font-size: 1rem;
+    width: 44%;
+}
+
+.register-button {
+    /* background-color: #007bff; */
+    background-color: rebeccapurple;
+}
+
+.inquiry-button {
+    /* background-color: #28a745; */
+    background-color: skyblue;
+}
+
+.sidebar {
+    position: sticky;
+    top: 100px;
+    width: 400px; 
+    /* height: 100px; */
+    background-color: #f4f4f4;
+    padding: 20px;
+    box-shadow: -2px 0 5px rgba(0, 0, 0, 0.2);
+    /* align-items: left; */
+    float: right;
+}
+.sidebar p {
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
---
+++ Frontend/HangUpT/src/components/Events.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="carousel-wrapper">
+    <Carousel :items-to-show="4.5" :wrap-around="false" :autoplay="false" transition="500">
+        <Slide v-for="item in list" :key="item.id">
+        <div class="slide_item">
+            <img :src="getImageUrl(item.image)" class="slide-image" />
+            <div class="slide-content">
+            <p class="slide-date">{{ item.date }}</p>
+            <h2 class="slide-title">{{ item.title }}</h2>
+            <h4 class="slide-price">{{ item.price }}</h4>
+            </div>
+        </div>
+        </Slide>
+
+        <template #addons>
+        <!-- <div class="navigation-container">
+            <Navigation />
+        </div> -->
+        </template>
+    </Carousel>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Slide } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
+import data from '../assets/list.js'
+
+export default defineComponent({
+    name: 'WrapAround',
+    components: {
+        Carousel,
+        Slide,
+        Navigation,
+    },
+    data(){
+        return {
+            list : data
+        }
+    },
+    methods:{
+            getImageUrl(url) {
+                return new URL(`${url}`, import.meta.url).href;
+            }
+    },
+})
+</script>
+
+<style>
+.carousel-container {
+    position: relative;
+    width: 100%;
+}
+    
+.slide-item {
+    position: relative;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+    
+.slide-image {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+}
+    
+.slide-overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.4);
+    color: white;
+    padding: 15px;
+    box-sizing: border-box;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+}
+
+.slide-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.slide-date {
+    font-size: 14px;
+    color: #b0b0b0;
+    margin-bottom: 5px;
+}
+
+.slide-title {
+    font-size: 18px;
+    margin: 0;
+    font-weight: bold;
+}
+
+.slide-price {
+    font-size: 14px;
+    font-weight: bold;
+    color: blueviolet;
+    margin-top: 20px;
+}
+
+</style>
