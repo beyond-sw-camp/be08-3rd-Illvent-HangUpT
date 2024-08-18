ec1d29fbe0e65432fb641dfa73f2f1820312c911
kuk329 <kuk329@cau.ac.kr>
2024-08-18 12:58:51 +0900

    feat : (front) 필터별 행사 목록 조회 페이지

--- Frontend/HangUpT/src/router/router.js
+++ Frontend/HangUpT/src/router/router.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from "vue-router";
 import Main from "../components/Main.vue";
 import Board from "../components/Board.vue";
 import BoardDetail from "../components/BoardDetail.vue";
+import EventView from "../views/EventView.vue";
 
 const routes = [
     { path: "/", component: Main },
     { path: "/boards", component: Board },
     { path: "/boards/:id", component: BoardDetail },
+    { path:"/events",name:'events',component: EventView}
 ];
 
 const router = createRouter({
